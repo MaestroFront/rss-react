940b1699fa85a98e6110a1a7f488e5f9b440bfea
Maestro <akievaleksandr@mail.ru>
2023-03-20 20:01:29 +0500

    feat: add routing

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.querySelector('.body') as HTMLDivElement);
+const root = ReactDOM.createRoot(
+  // eslint-disable-next-line prettier/prettier
+  document.querySelector('.body') as HTMLDivElement
+);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
