78ef1943bfb3bb244fa2565c37015ccbbfc6e2d0
Maestro <akievaleksandr@mail.ru>
2023-03-20 20:09:57 +0500

    feat: add error, aboutus pages and navigation + footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import './App.css';
 import React from 'react';
-import { Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import MainPage from './pages/Main';
+import ErrorPage from './pages/Error';
+import AboutUs from './pages/AboutUs';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
+      <Navigation />
       <main className="main">
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/about-us" element={<AboutUs />}></Route>
+          <Route path="*" element={<ErrorPage />}></Route>
+        </Routes>
       </main>
+      <Footer />
     </>
   );
 }
