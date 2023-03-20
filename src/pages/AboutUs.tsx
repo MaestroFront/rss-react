563ae440a5d901db4b4b12afcaf9bd9b782796aa
Maestro <akievaleksandr@mail.ru>
2023-03-20 20:26:38 +0500

    fix: update aboutus page

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const AboutUs = function () {
-  return <div>AboutUs</div>;
+  return (
+    <ul className="aboutus-container">
+      <h1 className="aboutus-title">Celebrity Vault</h1>
+      <li className="aboutus-text aboutus-text-n1">
+        This site is necessary for our world and for you
+      </li>
+      <li className="aboutus-text aboutus-text-n2">This is what you and your memories need</li>
+      <li className="aboutus-text aboutus-text-n3">
+        Dont forget who you grew up with and who you wanted to be like
+      </li>
+      <li className="aboutus-text aboutus-text-n4">These people are the top of everything</li>
+      <li className="aboutus-text aboutus-text-n5">
+        They are not in the past, they are not in the present, they are in your heart, they are
+        always there
+      </li>
+    </ul>
+  );
 };
 
 export default AboutUs;
